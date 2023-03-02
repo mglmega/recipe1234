587476602d999ff78b44be612224d96987e06e3d
mglmega <migartr@gmail.com>
2023-03-02 19:50:03 +0800

    remove developer mode fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // mode: 'development',
+  mode: 'production', //development
   entry: './src/js/index.js',
   output: {
     filename: 'js/main.js',
